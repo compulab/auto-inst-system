f764a2fae40a09453080a87f19618af53754d661
Uri Mashiach <uri.mashiach@compulab.co.il>
2016-05-10 11:23:53 +0300

    50-userspace-update.sh: update disk mount parameters chage
    The updated disk modified to ext2 image instead of a directory.
    Use loop mount for ext2 image file.

--- S50-userspace-update.sh
+++ S50-userspace-update.sh
@@ -9,7 +9,7 @@ UPDATE_FS=rootfs-update
 printk_config=$(cat /proc/sys/kernel/printk)
 echo "Mounting update disk ..."
 echo $PRINTK_NONE > /proc/sys/kernel/printk
-mount /media/source/$UPDATE_FS /media/rootfs/usr/local/mydebs/
+mount -o loop /media/source/$UPDATE_FS /media/rootfs/usr/local/mydebs/
 echo $printk_config > /proc/sys/kernel/printk
 echo "Update user space file system..."
 mount -t proc none /media/rootfs/proc
