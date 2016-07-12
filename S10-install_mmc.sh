1aa0238d1793be88b3d5bad197b79e3eaa1cadbb
Nikita Kiryanov <nikita@compulab.co.il>
2016-07-12 17:06:39 +0300

    S10-install_mmc: parameterize the script
    Create variables for key pieces of data as a first step towards
    genericity of the script.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

--- S10-install_mmc.sh
+++ S10-install_mmc.sh
@@ -2,36 +2,43 @@
 
 ##### Constants #####
 PRINTK_NONE="1 1 1 1"
-
-##### Main #####
-# Save printk configuration
 printk_config=$(cat /proc/sys/kernel/printk)
+
+SOURCE_MEDIA=/dev/mmcblk1p1
+SOURCE_MOUNT_PATH=/media/source
+DESTINATION_MEDIA=/dev/mmcblk0
+DESTINATION_KERNEL_MEDIA=/dev/mmcblk0p1
+DESTINATION_KERNEL_MOUNT_PATH=/media/boot
+FILESYSTEM_ARCHIVE_NAME=debian-armhf-image.tar.bz2
+DESTINATION_FILESYSTEM_MEDIA=/dev/mmcblk0p2
+DESTINATION_FILESYSTEM_MOUNT_PATH=/media/rootfs
+##### Main #####
 echo "Install..."
 echo "Updating partitions..."
 echo $PRINTK_NONE > /proc/sys/kernel/printk
-mdev -s && umount /dev/mmcblk0p1 1>&- 2>&- && umount /dev/mmcblk0p2 1>&- 2>&-
+mdev -s && umount ${DESTINATION_KERNEL_MEDIA} 1>&- 2>&- && umount ${DESTINATION_FILESYSTEM_MEDIA} 1>&- 2>&-
 echo $printk_config > /proc/sys/kernel/printk
-echo -e "o\nn\np\n1\n2048\n204800\na\n1\nt\nc\nn\np\n2\n204801\n\nw\neof\n" | fdisk -u /dev/mmcblk0 > /dev/null
+echo -e "o\nn\np\n1\n2048\n204800\na\n1\nt\nc\nn\np\n2\n204801\n\nw\neof\n" | fdisk -u ${DESTINATION_MEDIA} > /dev/null
 # Refresh the device nodes
 echo $PRINTK_NONE > /proc/sys/kernel/printk
-mdev -s 1>&- 2>&- && umount /dev/mmcblk0p1 1>&- 2>&- && umount /dev/mmcblk0p2 1>&- 2>&-
+mdev -s 1>&- 2>&- && umount ${DESTINATION_KERNEL_MEDIA} 1>&- 2>&- && umount ${DESTINATION_FILESYSTEM_MEDIA} 1>&- 2>&-
 echo $printk_config > /proc/sys/kernel/printk
 echo "Format partitions ..."
 ln -sf /proc/mounts /etc/mtab
-mkfs.vfat -n boot /dev/mmcblk0p1 > /dev/null
-mkfs.ext4 /dev/mmcblk0p2 1>&- 2>&-
+mkfs.vfat -n boot ${DESTINATION_KERNEL_MEDIA} > /dev/null
+mkfs.ext4 ${DESTINATION_FILESYSTEM_MEDIA} 1>&- 2>&-
 echo "Mounting partitions ..."
 # Mount source partition
-mkdir -p /media/source && mount /dev/mmcblk1p1 /media/source
+mkdir -p ${SOURCE_MOUNT_PATH} && mount ${SOURCE_MEDIA} ${SOURCE_MOUNT_PATH}
 # Mount boot partition
-mkdir -p /media/boot && mount /dev/mmcblk0p1 /media/boot
+mkdir -p ${DESTINATION_KERNEL_MOUNT_PATH} && mount ${DESTINATION_KERNEL_MEDIA} ${DESTINATION_KERNEL_MOUNT_PATH}
 # Mount root partition
 echo $PRINTK_NONE > /proc/sys/kernel/printk
-mkdir -p /media/rootfs && mount /dev/mmcblk0p2 /media/rootfs
+mkdir -p ${DESTINATION_FILESYSTEM_MOUNT_PATH} && mount ${DESTINATION_FILESYSTEM_MEDIA} ${DESTINATION_FILESYSTEM_MOUNT_PATH}
 echo $printk_config > /proc/sys/kernel/printk
 echo "Copy kernel files ..."
-cp /media/source/*.dtb /media/boot && sync
-cp /media/source/zImage* /media/boot && sync
+cp ${SOURCE_MOUNT_PATH}/*.dtb ${DESTINATION_KERNEL_MOUNT_PATH} && sync
+cp ${SOURCE_MOUNT_PATH}/zImage* ${DESTINATION_KERNEL_MOUNT_PATH} && sync
 echo "Extract user space ..."
-tar --numeric-owner -xvpjf /media/source/debian-armhf-image.tar.bz2 -C /media/rootfs > /dev/null && sync
 
+tar --numeric-owner -xvpjf ${SOURCE_MOUNT_PATH}/${FILESYSTEM_ARCHIVE_NAME} -C ${DESTINATION_FILESYSTEM_MOUNT_PATH} > /dev/null && sync
