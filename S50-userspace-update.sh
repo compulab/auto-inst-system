69c989fc73fcf79ee8f393e1ba1dd60477e5ff01
Uri Mashiach <uri.mashiach@compulab.co.il>
2016-07-14 16:58:53 +0300

    Semantic printing functions library in S50-userspace-update.sh
    Replace the echo messages with the semantic printing functions from
    printing_functions.sh
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

--- S50-userspace-update.sh
+++ S50-userspace-update.sh
@@ -4,14 +4,18 @@
 PRINTK_NONE="1 1 1 1"
 UPDATE_FS=rootfs-update
 
+##### External Scripts #####
+. "/etc/init.d/printing_functions.sh"
+
 ##### Main #####
 # Save printk configuration
 printk_config=$(cat /proc/sys/kernel/printk)
-echo "Mounting update disk ..."
+title "Updating user space"
+announce "Mounting update disk"
 echo $PRINTK_NONE > /proc/sys/kernel/printk
 mount -o loop /media/source/$UPDATE_FS /media/rootfs/usr/local/mydebs/
 echo $printk_config > /proc/sys/kernel/printk
-echo "Update user space file system..."
+announce "Installing packages"
 mount -t proc none /media/rootfs/proc
 mount -o bind /dev /media/rootfs/dev
 chroot /media/rootfs /usr/local/mydebs/install.sh > /dev/null 2>&1
