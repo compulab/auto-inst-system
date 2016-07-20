8a11a58f4cdd5cc081a52f9713c707f70417f176
Valentin Raevsky <valentin@compulab.co.il>
2016-07-20 15:39:49 +0300

    Add imx6 bootscr update script.
    Add imx6 bootscr update script.
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

---
+++ platform/imx6/S50-bootscr-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xv
+
+echo "Running $BASH_SOURCE with $@"
+
+BOARD_PARAM=/etc/init.d/board_params.sh
+
+[ ! -f ${BOARD_PARAM} ] && exit 1
+. ${BOARD_PARAM}
+
+[ -z $DESTINATION_KERNEL_MEDIA ] && exit 2
+
+boot_scr=$([[ ${DESTINATION_KERNEL_MEDIA} =~ "mmc" ]] &&  echo "boot.mmc.scr" || echo "boot.sata.scr" )
+
+stat $boot_scr &>/dev/null
+[ $? -ne 0 ] && exit 3
+
+TMP_DIR=$(mktemp -du)
+mkdir -p ${TMP_DIR} && mount ${DESTINATION_KERNEL_MEDIA} ${TMP_DIR}
+cp -v  $boot_scr ${TMP_DIR}/boot.scr 
+umount ${TMP_DIR}
+rm -rf ${TMP_DIR}
