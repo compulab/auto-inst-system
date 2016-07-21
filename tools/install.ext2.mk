d82a14a72d9061c14c8c1bb39f8b86a940d68af0
Valentin Raevsky <valentin@compulab.co.il>
2016-07-21 15:27:13 +0300

    install.ext2.mk: change the source files directory
    Change the source files directory.
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- tools/install.ext2.mk
+++ tools/install.ext2.mk
@@ -26,6 +26,6 @@ sudo hdparm -z ${device}
 sleep 0.5
 mpoint=$(udisks --mount ${device} | awk '$0=$NF')
 [ $? -eq 0 ] || exit 2
-sudo cp -v common/*.sh platform/${platform}/*.sh ${mpoint}
+sudo cp -v common/ext2.loop/* platform/${platform}/ext2.loop/* ${mpoint}
 udisks --unmount ${device}
 sudo losetup -d ${device}
