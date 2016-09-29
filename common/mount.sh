c2d63828c4e9d8693cd78760a0c612a9419e0b75
Uri Mashiach <uri.mashiach@compulab.co.il>
2016-09-29 16:35:14 +0300

    mount: remove unused code
    Remove functions: mount_partitions, unmount_partitions
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

--- common/mount.sh
+++ common/mount.sh
@@ -56,30 +56,6 @@ unmount_partitions() {
 	[ ! -z ${SOURCE_MEDIA} ] && umount -l ${SOURCE_MEDIA}
 }
 
-mount_partitions() {
-	announce "Mounting partitions"
-	# Mount source partition
-	if [ ! -z "${SOURCE_MEDIA}" ];then
-	mkdir -p ${SOURCE_MOUNT_PATH} && mount ${SOURCE_MEDIA} ${SOURCE_MOUNT_PATH}
-	fi
-	# Mount order is important
-	# Mount root partition
-	if [ ! -z "${DESTINATION_FILESYSTEM_MEDIA}" ];then
-	mkdir -p ${DESTINATION_FILESYSTEM_MOUNT_PATH} && mount ${DESTINATION_FILESYSTEM_MEDIA} ${DESTINATION_FILESYSTEM_MOUNT_PATH}
-	fi
-	# Mount boot partition onto the rootfs/boot
-	if [ ! -z ${DESTINATION_KERNEL_MEDIA} ];then
-	mkdir -p ${DESTINATION_KERNEL_MOUNT_PATH} && mount ${DESTINATION_KERNEL_MEDIA} ${DESTINATION_KERNEL_MOUNT_PATH}
-	fi
-}
-
-unmount_partitions() {
-	announce "Unmounting partitions"
-	[ ! -z ${DESTINATION_KERNEL_MEDIA} ] && umount -l ${DESTINATION_KERNEL_MEDIA}
-	[ ! -z ${DESTINATION_FILESYSTEM_MEDIA} ] && umount -l ${DESTINATION_FILESYSTEM_MEDIA}
-	[ ! -z ${SOURCE_MEDIA} ] && umount -l ${SOURCE_MEDIA}
-}
-
 mount_destination_nand() {
 	ubi_root=" -t ubifs ubi${UBI_DEV_ROOT}:${UBI_VOLNAME_ROOT}"
 	# Overwright DESTINATION parameters for NAND update
