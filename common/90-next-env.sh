7297602a22185344b6bad50696807ee792a748b9
Uri Mashiach <uri.mashiach@compulab.co.il>
2017-12-20 10:04:48 +0200

    environment: move U-Boot environment changes to Linux
    Current U-Boot environment changes are mainly made from U-Boot scripts
    which is problematic from the failure POV.
    Auto installs system should be able to use fw_setenv to adjust the U-Boot
    environment and this is accordingly to success/failure case.
    
    Modifications:
    * Add common script to handle the U-Boot environment update.
    The script executed before the un-mount process.
    * Remove the U-Boot update from the platform's boot script.
    * Add configuration file per platform. the configuration file includes
      environment related information.
    * Add work around file for platforms that requires special environment
      variables.
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

---
+++ common/90-next-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Automatic installation system
+#
+# Copyright (C) 2017 CompuLab, Ltd.
+# Author: Uri Mashiach <uri.mashiach@compulab.co.il>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or later
+# version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# Extract the U-Boot environment configuration section from the configuration file
+sed '1,/^\[ fw_env.config/d;/^\[/,$d' /media/source/config > /etc/fw_env.config
+# Extract the installation media boot command
+first_boot=$( awk '/^first_boot/ { gsub(/.*=/, "", $1); print }' /media/source/config )
+# Extract the environment unlock device
+unlock_dev=$( awk '/^unlock_dev/ { gsub(/.*=/, "", $1); print }' /media/source/config )
+# Set environment variables bootcmd and bootcmd_next
+[ -z "$unlock_dev" ] || flash_unlock $unlock_dev 0
+fw_setenv bootcmd_next `fw_printenv -n bootcmd`
+[ -z "$unlock_dev" ] || flash_unlock $unlock_dev 0
+fw_setenv bootcmd "setenv bootcmd \"\$bootcmd_next\"; setenv bootcmd_next; saveenv; $first_boot"
+[ -z "$unlock_dev" ] || flash_unlock $unlock_dev 0
+# Update U-Boot environment
+if [ -f /media/source/tmp-env-wa ]; then
+	awk '{ key = $1; gsub(/=.*/, "", key); gsub(/.*=/, "", $1); command = "fw_setenv " key " \47" $0 "\47"; print command | "/bin/sh"}' /media/source/tmp-env-wa
+	[ -z "$unlock_dev" ] || flash_unlock $unlock_dev 0
+fi
