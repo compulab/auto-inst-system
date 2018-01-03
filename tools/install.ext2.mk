2fcc972f8d7d67292da6d168caf4d2b0d9907d7e
Uri Mashiach <uri.mashiach@compulab.co.il>
2018-01-03 09:42:58 +0200

    common: remove the platform directory support
    The platform directory includes boot script and installation scripts
    which are relevant for specific platforms.
    
    All of the platform parameters were transferred to the platform
    configuration files.
    
    Remove the platform directory support from the tools scripts.
    Includes the binary output of the tools which are common to all of the
    platforms.
    
    Signed-off-by: Uri Mashiach <uri.mashiach@compulab.co.il>

--- tools/install.ext2.mk
+++ tools/install.ext2.mk
@@ -15,17 +15,14 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-common=$(dirname $BASH_SOURCE)/common.sh
-[ -f ${common} ] && . ${common}
-
-installer=install.ext2.${platform}
+installer=install.ext2
 size_inm=1
 mpoint=$(mktemp -d)
 dd if=/dev/zero of=${installer} count=${size_inm} bs=1M
-echo "y" | mkfs.ext2 -L installer.${platform} ${installer}
+echo "y" | mkfs.ext2 -L installer ${installer}
 sudo mount -o loop ${installer} ${mpoint}
 if  [ $? -eq 0 ];then
-	for src in common platform/${platform};do
+	for src in common;do
 		sudo cp -v ${src}/* ${mpoint} 2>/dev/null
 	done
 	sudo umount -l ${mpoint}
